76fd8a3ece91c24464c6a734623c0c979e936692
Kameron Freitag <18662255+KameronFreitag@users.noreply.github.com>
2018-08-24 19:20:13 -0500

    Added JSON Serialization
    Added some JSON serialization to the project
    The project currently outputs a .JSON list a of the Top 10 Players using statistics gained from a .JSON file in the main directory.

---
+++ SoccerStats/Player.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerStats
+{
+
+    public class RootObject
+    {
+        public Player[] Players { get; set; }
+    }
+
+    public class Player
+    {
+        //This thing in the brackets is an attribute
+        //On it's own, it does nothing
+        //Coupled with the code below, it can describe what to do with some data
+        //Consider it meta data
+        [JsonProperty(PropertyName = "first_name")]
+        public string FirstName { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+        [JsonProperty(PropertyName = "points_per_game")]
+        public double PointsPerGame { get; set; }
+        [JsonProperty(PropertyName = "second_name")]
+        public string SecondName { get; set; }
+        [JsonProperty(PropertyName = "team_name")]
+        public string TeamName { get; set; }
+    }
+}
--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace SoccerStats
 {
@@ -17,6 +18,20 @@ namespace SoccerStats
         //Append our current directory to the document we want to read so we have the full file path
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
             var fileContents = ReadSoccerResults(fileName);
+        //We have a JSON document in our files.  Let's DESERIALIZE it.
+        //To do so we must read from the file.  Let's change that file name!
+            fileName = Path.Combine(directory.FullName, "players.json");
+            var players = DeserializePlayers(fileName);
+            var topTenPlayers = GetTopTenPlayers(players);
+
+            foreach(var player in topTenPlayers)
+            {
+                Console.WriteLine("{0} {1}\n\tTeam: {2}\n\tPoints: {3}", player.FirstName, player.SecondName, player.TeamName, player.PointsPerGame);
+            }
+
+            //We're going to serialize our output as a .json file
+            fileName = Path.Combine(directory.FullName, "topten.json");
+            SerializePlayersToFile(topTenPlayers, fileName);
         }
 
         public static string ReadFile(string fileName)
@@ -83,6 +98,45 @@ namespace SoccerStats
             }
 
             return soccerResults;
+        }
+
+        public static List<Player> DeserializePlayers(string fileName)
+        {
+            var players = new List<Player>();
+            var serializer = new JsonSerializer();
+            using (var reader = new StreamReader(fileName))
+            using(var jsonReader = new JsonTextReader(reader))
+            {
+                //Deserialize<T> can return whatever info you pass it as whatever type you tell it to in T (The type parameter)
+                //For example, we're giving it an array of strings but it's going to convert it to a List of Player objects
+                players = serializer.Deserialize<List<Player>>(jsonReader);
+            }
+                
+            return players;
+        }
+
+        public static List<Player> GetTopTenPlayers(List<Player> players)
+        {
+            //var topTenPlayers = new List<Player>();
+            //players.Sort(new PlayerComparer());
+
+            //Using a linq statement to return a sorted list of 10
+            return (
+                from player in players
+                orderby player.PointsPerGame descending
+                select player).Take(10).ToList();
+        }
+
+        public static void SerializePlayersToFile(List<Player> players, string fileName)
+        {
+            var serializer = new JsonSerializer();
+            using (var writer = new StreamWriter(fileName))
+            using (var jsonWriter = new JsonTextWriter(writer))
+            {
+                //Deserialize<T> can return whatever info you pass it as whatever type you tell it to in T (The type parameter)
+                //For example, we're giving it an array of strings but it's going to convert it to a List of Player objects
+                serializer.Serialize(jsonWriter, players);
+            }
         }
     }
 }
